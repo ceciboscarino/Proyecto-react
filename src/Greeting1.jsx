05d91d2c5e9b1d9866f668fc53e6448774aa391c
ceciboscarino <ceciboscarino@hotmail.com>
2023-02-02 17:26:51 -0300

    aprendiendo react

---
+++ src/Greeting1.jsx
@@ -0,0 +1,12 @@
+ export function Greeting1() {
+    return (
+      <div>
+        <h1> Este es un componente</h1>
+        <p> Lorem 123</p>
+      </div>
+    );
+  }
+
+  export function Usuario() {
+    return <h1>Usuario</h1>
+  }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import { Greeting1, Usuario } from "./Greeting1";
+import Producto from "./Producto";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDom.createRoot(document.getElementById("root"));
+
+// 3 formas distintas de llamar una funcion (greeting)
+
+function Greeting2() {
+  const married = false;
+  return <h1>{married ? "estoy casado" : "no estoy casado"} </h1>;
+}
+
+function Greeting3() {
+  const user = {
+    firstname: "Joe",
+    lastname: "Macmillan",
+  };
+  return (
+    <div>
+      <h1>{user.firstname}</h1>
+      <h3>{user.lastname}</h3>
+    </div>
+  );
+}
+
+function Greeting4() {
+  function add(x, y) {
+    return x + y;
+  }
+  return <h1> {add(10, 30)}</h1>;
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <Greeting1 />
+    <Greeting2></Greeting2>
+    {Greeting3()}
+    <Greeting4 />
+    <Usuario />
+    <Producto />
+  </>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
